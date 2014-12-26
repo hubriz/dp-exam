20203040e120c4efaa46d51ae7d6fee982d4f868
hubriz <killthepiper@gmail.com>
2014-12-26 15:16:43 +0800

    Added HTML files and assets

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+
+/* -------------------------------------------------------------
+    PSD to HTML
+    ===================================================
+
+    Converted by ART DE GUZMAN
+    26 September 2013
+    @hubriz
+    
+-------------------------------------------------------------- */
+
+(function($){
+  
+  // Load IE not supported
+  jQuery('#ie-drop').modal('show');
+  
+  // Homepage Carousel
+  var homeCarousel = jQuery('#carousel-home .flexslider');
+    
+  homeCarousel.flexslider({animation:'fade'});
+  
+    
+})(jQuery);
